9e8b1420870c438c519940a36c7dbfd99575d76e
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2023-05-09 16:35:40 -0400

    display image and dimension

--- pca_tests.py
+++ pca_tests.py
@@ -19,13 +19,19 @@ for i in range(len(files)):
 
     train_labels[i] = tools.rgb_to_grayscale(train_labels[i])
 
+    tools.show_imgs(train_labels[i])
+
+    train_labels[i] = tools.flatten_imgs(train_labels[i])
 
 
     pca = PCA(n_components)
-    new_val = pca.fit_transform(np.asarray(train_labels[i]))
 
-    cv2.imshow(f'image{i}', new_val[0])
-    cv2.waitKey(0)
+    pca.fit(train_labels[i])
+
+    tools.show_imgs(pca.components_[:n_components])
+
+    #new_val = pca.fit_transform(np.asarray(train_labels[i]))
+    #tools.show_imgs(new_val)
 
 
 
