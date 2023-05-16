8368c4acdbc4ba24d56b1c21ecb1346e2f113486
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2023-05-16 09:16:14 -0400

    bug fix

--- pca_tests.py
+++ pca_tests.py
@@ -2,36 +2,99 @@ import os
 import cv2
 import tools
 import numpy as np
-from sklearn.decomposition import PCA
-
-labeled_img_path = "C:\Dennis\Personal\Projects\ml_playground\data\VIP_2022_fall_classification\Train"
-num_imgs = 100
-n_components = 5
-
-train_labels = tools.img_dir_to_numpy_arr(labeled_img_path, num_imgs)
-
-files = os.listdir(labeled_img_path)
-
-img_arr = {}
-
-for i in range(len(files)):
-    img_arr[files[i]] = train_labels[i]
-
-    train_labels[i] = tools.rgb_to_grayscale(train_labels[i])
-
-    tools.show_imgs(train_labels[i])
-
-    train_labels[i] = tools.flatten_imgs(train_labels[i])
+from sklearn.decomposition import PCA, KernelPCA
 
 
-    pca = PCA(n_components)
 
-    pca.fit(train_labels[i])
+def kernel_pca_by_class(num_imgs, labeled_img_path, n_components):
 
-    tools.show_imgs(pca.components_[:n_components])
+    '''
 
-    #new_val = pca.fit_transform(np.asarray(train_labels[i]))
-    #tools.show_imgs(new_val)
+    :param num_imgs:
+    :param labeled_img_path:
+    :param n_components:
+    :return:
+    '''
+
+    train_labels = tools.img_dir_to_numpy_arr(labeled_img_path, num_imgs)
+
+    files = os.listdir(labeled_img_path)
+
+    img_dict ={}
+
+    kernel_pca_result = []
+
+    for i in range(len(files)):
+        img_dict[files[i]] = train_labels[i]
+        train_labels[i] = tools.rgb_to_grayscale(train_labels[i])
+        flattened_labels = tools.flatten_imgs((train_labels[i]))
+        kernel_pca = KernelPCA(n_components=n_components, kernel="rbf", fit_inverse_transform=True)
+        kernel_pca_result.append(flattened_labels)
+        ##
+        x_transformed = kernel_pca.fit_transform(flattened_labels)
+        kernel_pca_result.append(kernel_pca.inverse_transform(x_transformed))
+
+    #tools.show_compare_imgs(kernel_pca_result)
+    return kernel_pca_result
+
+def pca_by_class(num_imgs, labeled_img_path, n_components):
+
+    train_labels = tools.img_dir_to_numpy_arr(labeled_img_path, num_imgs)
+
+
+    files = os.listdir(labeled_img_path)
+
+    img_dict = {}
+
+    pca_result = []
+
+    ## Regular PCA on each class
+
+    for i in range(len(files)):
+        img_dict[files[i]] = train_labels[i]
+        train_labels[i] = tools.rgb_to_grayscale(train_labels[i])
+        flattened_labels = tools.flatten_imgs(train_labels[i])
+        pca = PCA(n_components)
+        pca.fit(flattened_labels)
+        pca_result.append(pca.components_[:n_components])
+        #tools.show_compare_imgs([train_labels[i], pca.components_[:n_components]])
+
+    return pca_result
+
+
+def one_class_pca(num_imgs, labeled_img_path, n_components, type):
+
+
+    labeled_img_path = os.path.join(labeled_img_path, type)
+
+    print(labeled_img_path)
+
+    train_labels = tools.img_to_numpy_arr(labeled_img_path, num_imgs)
+
+    train_labels = tools.rgb_to_grayscale(train_labels)
+
+    flattened_labels = tools.flatten_imgs(train_labels)
+
+    kernel_pca = KernelPCA(n_components=n_components, kernel="rbf", fit_inverse_transform=True)
+
+    x_transformed = kernel_pca.fit_transform(flattened_labels)
+    #tools.show_imgs(kernel_pca.inverse_transform(x_transformed))
+    return kernel_pca.inverse_transform(x_transformed)
+
+
+if __name__ == '__main__':
+    labeled_img_path = "C:\Dennis\Personal\Projects\ml_playground\data\VIP_2022_fall_classification\Train"
+    #num_imgs = 1200
+    #n_components = 250
+
+    num_imgs = 100
+    n_components = 10
+
+    one_class_pca(num_imgs, labeled_img_path, n_components, type= 'buildings')
+
+   #kernel_pca_by_class(num_imgs, labeled_img_path, n_components)
+    #pca_by_class(num_imgs, labeled_img_path, n_components)
+
 
 
 
